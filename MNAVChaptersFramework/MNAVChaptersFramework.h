939eff35ac974619a5ece6d8e613d007d826311d
Kyle Hankinson <kyle@hankinsoft.com>
2018-09-04 20:05:24 -0300

    + Framework for use via Carthage

---
+++ MNAVChaptersFramework/MNAVChaptersFramework.h
@@ -0,0 +1,19 @@
+//
+//  MNAVChaptersFramework.h
+//  MNAVChaptersFramework
+//
+//  Created by kylehankinson on 2018-09-04.
+//  Copyright © 2018 Michael Nisi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MNAVChaptersFramework.
+FOUNDATION_EXPORT double MNAVChaptersFrameworkVersionNumber;
+
+//! Project version string for MNAVChaptersFramework.
+FOUNDATION_EXPORT const unsigned char MNAVChaptersFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MNAVChaptersFramework/PublicHeader.h>
+
+#import "MNAVChapterReader.h"
